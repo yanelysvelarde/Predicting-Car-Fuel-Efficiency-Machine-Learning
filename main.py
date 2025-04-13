9707f99b0246e21768872546311cb207b163d06a
yanelysvelarde <yanelys932@gmail.com>
2025-04-13 17:26:07 -0400

    Put everything into one file, created the 3 models, run model_trainer.py

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-import transformers
-import torch
-import numpy
+import joblib
+import pandas as pd
+
+#load saved model
+model = joblib.load("random_forest_model.pkl")
 
 
-def main():
-  pass
-  
+input_data = pd.DataFrame([{
+    'displ': 2.5,
+    'cylinders': 4,
+    'year': 2020,
+    'drive_4-Wheel Drive': 0,
+    'drive_All-Wheel Drive': 1,
+    'drive_Front-Wheel Drive': 0,
+    'drive_Part-time 4-Wheel Drive': 0,
+    'drive_Rear-Wheel Drive': 0
+}])
 
-if __name__ == "__main__":
-  main()
+#predict MPG
+predicted_mpg = model.predict(input_data)
+print(f"Predicted MPG: {predicted_mpg[0]:.2f}")
+
+
+
